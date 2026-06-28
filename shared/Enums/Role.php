319027bad8005a8365caf1c83d3bd141d9a09c48
Arman <407448+armanist@users.noreply.github.com>
2026-06-28 22:55:37 +0400

    [#197] Align project metadata with PHP 8 baseline

--- shared/Enums/Role.php
+++ shared/Enums/Role.php
@@ -4,14 +4,8 @@ declare(strict_types=1);
 
 /**
  * Quantum PHP Framework
- *
- * An open source software development framework for PHP
- *
- * @package Quantum
- * @author Arman Ag. <arman.ag@softberg.org>
- * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
- * @link http://quantum.softberg.org/
- * @since 3.0.0
+ * An open-source software development framework for PHP
+ * @link https://quantumphp.io
  */
 
 namespace Shared\Enums;
